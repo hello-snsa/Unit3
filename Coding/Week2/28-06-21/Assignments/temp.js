153b83813ae044b0a35164345a7d740777faf843
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-06-29 13:43:29 +0530

    Create temp.js

---
+++ Coding/Week2/28-06-21/Assignments/temp.js
@@ -0,0 +1,28 @@
+var xyz= function (resolve,reject){
+    resolve(data);
+   reject('rejeccted')
+
+
+};
+
+var myPromise= new Promise(xyz);
+
+
+
+
+
+
+
+myPromise.then(
+    function(res)
+{
+//print
+//sub
+//whatever +res
+}
+.catch
+
+
+){
+    //
+}
