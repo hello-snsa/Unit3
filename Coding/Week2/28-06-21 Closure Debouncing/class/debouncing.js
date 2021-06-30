f68c9cfa83c78a5e59411525d2a0eb28b0b4dfec
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-06-30 17:14:41 +0530

    Create debouncing.js

---
+++ Coding/Week2/28-06-21 Closure Debouncing/class/debouncing.js
@@ -0,0 +1,69 @@
+console.log("script debouncing.js is working")
+
+let movies_div = document.getElementById('movies')
+
+async function searchMovies() {
+    let name = document.getElementById('query').value;
+   
+    // if (name.length <= 2) {
+    //     return false;
+ 
+    // }
+
+    let response = await fetch(`https://www.omdbapi.com/?s=${name}&apikey=d806bd70`);
+
+    let data = await response.json();
+    // console.log('data: ',data)
+
+    return data;
+
+}
+     
+function appendMovies(movies) {
+
+    movies_div.innerText=null;
+    // movies.forEach((el) => {
+    movies.forEach(({Title}) => {
+        let p = document.createElement('p');
+        // p.innerText = el.Title;
+        p.innerText = Title;
+
+        movies_div.append(p);
+
+
+    })
+
+}
+
+async function main(){
+
+    // let data= await  searchMovies();
+    // appendMovies(data.Search);
+
+    let {Search}= await  searchMovies();
+    appendMovies(Search);
+
+}
+
+var timer_id;
+function throttleFunction(func,delay){
+   
+//If setTimeout is already there, no need to do anything.
+
+//a
+
+    if(timer_id){
+        return false;
+    }
+        timer_id = setTimeout(()=>{
+            func() //calling main() function
+
+            timer_id = undefined;
+
+        },delay);
+
+    }
+
+    //a main(),2000
+    //b main(),2000
+
