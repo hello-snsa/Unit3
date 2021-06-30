5e35f7c049f0f05fb76e0051a7e232cd05e04c37
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-06-30 17:15:13 +0530

    Update main.js

--- Coding/Week2/29-06-21/Class/main.js
+++ Coding/Week2/29-06-21/Class/main.js
@@ -19,17 +19,73 @@ function Register(e){
     {
         method: 'POST',
         body: formdata,
+        //mode:'no-cars',
+
         //additional information your request
 
         headers:{
-        'Content-Type':'application/json'
+        'Content-Type':'application/json',
 
-        }
+        },
 })
 .then((response) => {
     return response.json()
 }).then((response) => {
     console.log('response: ',response)
 
-}).catch
+}).catch((err) => {
+    console.log('err:',err)
+
+})
 }
+
+function Login(e){
+    e.preventDefault()
+
+    let formdata={
+        username:document.getElementById("inp7").value,
+        password:document.getElementById("inp8").value,
+    };
+    formdata= JSON.stringify(formdata);
+    
+    fetch("https://masai-api-mocker.herokuapp.com/auth/login",{
+
+        method: 'POST',
+        body: formdata,
+        
+
+        //additional information your request
+        headers:{
+        'Content-Type':'application/json',
+        },
+
+    }).then((response) => {
+
+        return response.json()
+    }).then((response) => {
+        console.log('response: ',response)
+    
+    }).catch((err) => {
+        console.log('err:',err)
+    
+    })
+
+    // fetch(input: RequestInfo, init?: RequestInit): Promise<Response>
+
+    // })
+}
+
+function getmyProfile(){
+
+fetch(`https://masai-api-mocker.herokuapp.com/user/${u}`,{
+
+headers:{
+    
+}
+
+})
+
+}
+
+
+//token: 4c9d33298e7898812521300c21bd7349
