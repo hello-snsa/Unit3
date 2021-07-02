4fcc62797e8e7156b75ee12fce0d70fe101f2a5e
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-02 18:47:41 +0530

    Create navbar.js

---
+++ Coding/Week2/01-07-21 NPM/Class/webpack-demo/components/navbar.js
@@ -0,0 +1,26 @@
+
+function navbar()
+{
+    return ` <div id="navbar">
+
+    <h3><a href="index.html">Home</a></h3>
+    
+    <div class="options">
+        
+        <h3><a href="jewellary.html">Jewelary</a></h3>
+        <h3><a href="electronics.html">Electronics</a></h3>
+        <h3><a href="#">Sign Up</a></h3>
+        <h3><a href="#">Sign In</a></h3>
+
+
+    </div>
+
+
+</div>
+
+<div id="cont" ></div>
+     
+    `
+}
+
+export default navbar;
