ba6fe3f721483fe8b2bcc4614e2ac2194ea480a0
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-02 18:48:20 +0530

    Update index.js

--- Coding/Week2/01-07-21 NPM/Class/webpack-demo/src/index.js
+++ Coding/Week2/01-07-21 NPM/Class/webpack-demo/src/index.js
@@ -1,21 +1,5 @@
-import axios from 'axios'
-
-
-// function component() {
-  // const element = document.createElement('div');
-
-  //making GET request using axios
-  // axios.get("https://fakestoreapi.com/products")
-  //   .then((res) => {
-  //     console.log('res:', res);
-  //     console.log("hello world");
-  //   });
-
-
-  // return element;
-// }
-
-// document.body.appendChild(component()); 
+import navbar from "../components/navbar.js";
+import axios from 'axios';
 
 async function getData(){
 
@@ -26,3 +10,21 @@ async function getData(){
 
 }
 getData();
+let nav= document.getElementById('navContainer');
+document.body.innerHTML=navbar();
+
+// function component() {
+  // const element = document.createElement('div');
+
+  //making GET request using axios
+  // axios.get("https://fakestoreapi.com/products")
+  //   .then((res) => {
+  //     console.log('res:', res);
+  //     console.log("hello world");
+  //   }); 
+ 
+
+  // return element;
+// }
+
+// document.body.appendChild(component()); 
