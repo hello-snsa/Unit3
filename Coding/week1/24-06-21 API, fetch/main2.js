8d8529714da6d83230a71cb4bd24225f4456c07b
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-02 22:27:35 +0530

    Create main2.js

---
+++ Coding/week1/24-06-21 API, fetch/main2.js
@@ -0,0 +1,44 @@
+// a5000c74f0241ce5e4ddc4481b7fd071
+// api.openweathermap.org/data/2.5/weather?q=London&appid={a5000c74f0241ce5e4ddc4481b7fd071}
+
+let container= document.getElementById('container');
+
+
+async function getWeatherData(){
+
+    var city = document.getElementById('cityName').value;
+    
+    try{
+    let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a5000c74f0241ce5e4ddc4481b7fd071`);
+
+   let data= await res.json();
+    console.log('data:',data)
+
+    showWeatherData(data);
+} catch(e){
+    console.log("e: ",e);
+    }
+
+    }
+
+
+
+function showWeatherData(d){
+    let name=document.createElement('p');
+    name.innerText=`name - ${d.name}`;
+
+
+    let temp=document.createElement('p');
+    temp.innerHTML=`Temp - ${Math.round(d.main.temp) - 273} C `;
+
+    let humidity=document.createElement('p');
+    humidity.innerText=`humidity - ${d.main.humidity}`;
+
+    let pressure=document.createElement('p');
+    pressure.innerText=`pressure - ${d.main.pressure}`;
+
+    
+    container.append(name,temp,humidity,pressure);
+
+}
+getWeatherData();
