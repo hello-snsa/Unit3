385f03d273fcd2ce0b3a5970d9f827636ce23157
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-04 16:58:04 +0530

    Update youtube2.js

--- Coding/Week2/28-06-21 Closure Debouncing ES6 I/Assignments/youtube2.js
+++ Coding/Week2/28-06-21 Closure Debouncing ES6 I/Assignments/youtube2.js
@@ -49,19 +49,27 @@ function enter(ev){
 
 async function initialVideos(){
 
-    let query= "trending";
     try{
-      var res= await  fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&key=AIzaSyC28lWQiG6_Ow_FcKB7aNyUWgzfnHrkfIY&maxResults=20`)
-    
+      // var res= await  fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&key=AIzaSyC28lWQiG6_Ow_FcKB7aNyUWgzfnHrkfIY&maxResults=20`)
+      // var res= await  fetch(`https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyC28lWQiG6_Ow_FcKB7aNyUWgzfnHrkfIY&maxResults=20`)
+      var res= await  fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=es&key=AIzaSyC28lWQiG6_Ow_FcKB7aNyUWgzfnHrkfIY`)
+      //&videoCategoryId=17
+
+try{
       let {items}= await res.json();
     items=items.filter(({id:{videoId}})=>{return videoId!=undefined})
     console.log(items)
     appendVideos(items)
     
-    }
+    
+  }catch(e){
+    console.log("ok");
+  }
+}
     catch(e){
         console.log('e:',e)
     }
+
 }
     
 
@@ -83,9 +91,4 @@ function homeVideos(videos){
 }
 
 initialVideos();
-homeVideos();
-
-
-{/* <iframe width="350" height="215" src="https://www.youtube.com/embed/6Dq6BHxx7cA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-<iframe width="350" height="215" src="https://www.youtube.com/embed/0W_qj2UHRd4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+// homeVideos();
