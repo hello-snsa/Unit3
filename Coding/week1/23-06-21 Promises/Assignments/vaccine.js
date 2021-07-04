0bf437fcd96be13617cf954ad8a6a873b78ace6f
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-04 09:05:43 +0530

    Update vaccine.js

--- Coding/week1/23-06-21 Promises/Assignments/vaccine.js
+++ Coding/week1/23-06-21 Promises/Assignments/vaccine.js
@@ -1,22 +1,46 @@
-let queue = ['abhishek', 'pulkit', 'chandan','manish', 'Debashish', 'sonam'];
+let queue = ['abhishek', 'pulkit', 'chandan','manish', 'Debashish', 'sonam','mani'];
+var vaccine_counter = document.getElementById("vaccine_counter");
+var patient_counter = document.getElementById("patient_counter");
+var counter=10;
+var pCount= queue.length;
+
+var pAge;
+
 
 function register() {
     // localStorage.setItem('queue',JSON.stringify(queue));
+    pCount++;
 
     let name = document.getElementById('name').value;
+    pAge=Number(document.getElementById("pAge").value);
+    
+    if(pAge>60)
+    queue.unshift(name);
+    else
     queue.push(name);
-    let message = `${name} Your registration is succesfull. Please wait for your turn.`;
 
+    console.log("queue ",queue)
+   
+    if(counter>=1)
+   { var message = `${name} Your registration is succesfull. Please wait for your turn.`;}
+
+    else{
+    message= 'vaccine out of stock, Register tomorrow';
+
+}
     alert(message);
 
     let promise = new Promise(function (resolve, reject) {
 
         setInterval(function (){
         
-        if (queue[0] == name) {
-
+        if (queue[0] == name && counter>=1) {
+            // counter++;
+           
+            console.log("counter",counter)
             resolve(`${name} its your turn now.`)
-            clearInterval(interval);
+
+            // clearInterval(interval);
         }
         // else{
         //     reject('error')
@@ -37,9 +61,47 @@ function register() {
 //step-2: To consistently remove peoplw from the queue
 
 function startVaccination(){
-
-queue.shift()
-console.log('queue: ',queue);
-
+if(pCount==0 && counter>0)
+{
+    wait();
 }
-let interval= setInterval(startVaccination,2000)
+
+    if(pCount<=0)
+    pCount=0;
+   else
+    pCount--;
+
+    if(counter>0){
+    queue.shift();
+    counter--;
+    vaccine_counter.innerHTML=counter;
+    patient_counter.innerHTML=pCount;
+    console.log('queue: ',queue);
+}
+else if(counter==0){
+    queue.shift();
+    // counter--;
+   
+    vaccine_counter.innerHTML=counter;
+    patient_counter.innerHTML=pCount;
+        alert("vaccine out of stock, visit tomorrow")
+        console.log(queue)
+        clearInterval(interval)
+        return;
+    }
+    else{
+        clearInterval(interval)
+    }
+   
+}
+let interval= setInterval(startVaccination,3000);
+
+function wait(){
+    
+    if(pCount>0){
+        return;
+    }
+    else{
+        wait();
+    }
+}
