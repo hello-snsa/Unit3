61409ed653eab65cfffc1c4ddd3cc9c4d0ba1ad1
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-06 19:17:46 +0530

    Create List.jsx

---
+++ Coding/Week 3/06-07-2021 Introduction to react/preclass/my-first-app/src/Components/List.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function List(){
+    return(
+
+        <div>
+            <ul>
+                <li>List 1</li>
+                <li>List 2</li>
+                <li>List 3</li>
+                <li>List 4</li>
+               
+            </ul>
+        </div>
+
+
+
+    )
+}
+
+export default List;
