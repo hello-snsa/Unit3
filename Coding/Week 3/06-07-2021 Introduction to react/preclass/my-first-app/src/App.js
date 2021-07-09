e8be87dc868b1892258cdb8f5fab952bc62c6bcb
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-09 11:01:24 +0530

    Update App.js

--- Coding/Week 3/06-07-2021 Introduction to react/preclass/my-first-app/src/App.js
+++ Coding/Week 3/06-07-2021 Introduction to react/preclass/my-first-app/src/App.js
@@ -1,19 +1,35 @@
 import './App.css';
 // import List from './Components/List';
+import Cards from './Components/Cards';
 import Cards1 from './Components/Cards1';
 import Cards2 from './Components/Cards2';
+import Counter from "./Components/Counter";
 
-// var header= "Mobile Operating System";
-// var below=['Android','blackberry','iphone','Windows Phone'] ;
+var header= "Mobile Operating System";
+var data=['Android','blackberry','iphone','Windows Phone'] ;
+
+var header2= "Mobile Manufacturers";
+var data2=['Samsung','HTC','Micromax','Apple'] ;
+
 
     function App(){
       return(
       
 
         <div>
-            <Cards1 /> 
+            <Cards header={header} data={data} /> 
+            <Cards header={header2} data={data2} /> 
 
-            <Cards2 />
+            {/* <Cards1 />
+            <Cards2 /> */}
+
+
+            <div>
+              <h2>counter</h2>
+              <div className="App">
+                <Counter />
+              </div>
+            </div>
     
           </div>
       
