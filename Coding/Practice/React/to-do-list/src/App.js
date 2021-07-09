4e1e0e0659e5e52acb9cd47a2e24b6fc90f8abf3
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-09 11:00:55 +0530

    Create App.js

---
+++ Coding/Practice/React/to-do-list/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+    <div className="mainDiv">
+      <div className="centerDiv">
+
+
+
+
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
