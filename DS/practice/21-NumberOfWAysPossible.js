d8dfdd05d2337a912458374d6e3781b6ae43795d
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-15 00:03:49 +0530

    Create 21-NumberOfWAysPossible.js

---
+++ DS/practice/21-NumberOfWAysPossible.js
@@ -0,0 +1,30 @@
+function runProgram(input) {
+
+  input=Number(input.trim());
+
+
+console.log(input)
+
+
+
+
+}//End of runProgram()
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = ``;
+process.stdin.on("data", function (input) {
+  read += input;
+});
+process.stdin.on("end", function () {
+  read = read.replace(/\n$/, "");
+  read = read.replace(/\n$/, "");
+  runProgram(read);
+});
+process.on("SIGINT", function () {
+  read = read.replace(/\n$/, "");
+  runProgram(read);
+  process.exit(0);
+});//End of program
+
+
