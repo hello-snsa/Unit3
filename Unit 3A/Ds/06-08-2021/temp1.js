72d959b7efce8f2018ed3d199f23be43d56abb63
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-08-07 10:15:01 +0530

    Create temp1.js

---
+++ Unit 3A/Ds/06-08-2021/temp1.js
@@ -0,0 +1,28 @@
+function runProgram(input) {
+
+    input = input.trim();
+    var newInput = input.split(" ").map(Number);
+    let num = newInput[0];
+    let pow = newInput[1];
+
+    console.log(num ** pow)
+
+
+}//End of runProgram()
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = ``;
+process.stdin.on("data", function (input) {
+    read += input;
+});
+process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+});
+process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+});//End of program
