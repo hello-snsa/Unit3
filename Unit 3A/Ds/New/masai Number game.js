a865e957cc31daedc1e5cc63e796e3200b483e02
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-08-29 23:35:31 +0530

    Update masai Number game.js

--- Unit 3A/Ds/New/masai Number game.js
+++ Unit 3A/Ds/New/masai Number game.js
@@ -1,44 +1,46 @@
 function runProgram(input) {
     input = input.trim();
 
-    let newInput = input.split("\n").map(Number);
+    var newInput = input.split("\n").map(Number);
 
-    let N = newInput[0];
-    let str = "";
-    let res;
+    var N = newInput[0];
+    var str = "";
+    var res;
+
+    const fun = (a, arr) => {
+        res = -1;
+        for (var i = 1; i < arr.length; i++) {
+            if (arr[i] < a) {
+                res = arr[i];
+                break;
+            }
+        }
+        return res;
+    }
+
+    const fun1 = (a, arr) => {
+        res = -1;
+        var max = -1;
+
+        for (var i = 1; i < arr.length; i++) {
+            if (arr[i] > a) {
+                max = arr[i];
+                res = fun(max, arr.slice(i))
+                break;
+            }
+        }
+        return res;
+    }
 
 
-    for (let a = 1; a < N; a++) {
+
+    for (var a = 1; a < N; a++) {
         res = fun1(newInput[a], newInput.slice(a));
         str += res + " "
     }
-    str = str + "-1"
-    console.log(str)
-}
 
-function fun1(a, arr) {
-    let max = -1;
-    res = -1;
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > a) {
-            max = arr[i];
-            res = fun2(max, arr.slice(i))
-            break;
-        }
-    }
-    return res;
-}
-
-function fun2(a, arr) {
-    res = -1;
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < a) {
-            res = arr[i];
-            break;
-        }
-    }
-    return res;
+    str += "-1"
+    console.log(str);
 
 
 }//End of runProgram()
@@ -59,3 +61,8 @@ process.on("SIGINT", function () {
     runProgram(read);
     process.exit(0);
 });//End of program
+
+
+
+
+
